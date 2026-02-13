a03ed718631870583e59395f43a8907e52fd19cb
chrissbarr <chris@chrisbarrbuilds.com>
2026-02-14 09:50:20 +1030

    Improve example comments

--- examples/differential/differential.ino
+++ examples/differential/differential.ino
@@ -18,14 +18,14 @@ void setup(void) {
   // functions, but be careful never to exceed VDD +0.3V max, or to
   // exceed the upper and lower limits if you adjust the input range!
   // Setting these values incorrectly may destroy your ADC!
-  //                                                                ADS1015  ADS1115
-  //                                                                -------  -------
-  // ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
-  // ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
-  // ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
-  // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
-  // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
-  // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
+  //                                                                       ADS1015  ADS1115
+  //                                                                       -------  -------
+  // ads.setGain(Gain::TWOTHIRDS_6144MV); // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
+  // ads.setGain(Gain::ONE_4096MV);       // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
+  // ads.setGain(Gain::TWO_2048MV);       // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
+  // ads.setGain(Gain::FOUR_1024MV);      // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
+  // ads.setGain(Gain::EIGHT_512MV);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
+  // ads.setGain(Gain::SIXTEEN_256MV);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
 
   ads.begin();
   ads.setGain(Gain::TWOTHIRDS_6144MV);
@@ -34,17 +34,13 @@ void setup(void) {
 void loop(void) {
   int16_t results;
 
-  /* Be sure to update this value based on the IC and the gain settings! */
-  float multiplier = 3.0F; /* ADS1015 @ +/- 6.144V gain (12-bit results) */
-  // float multiplier = 0.1875F; /* ADS1115  @ +/- 6.144V gain (16-bit results) */
-
   results = ads.readADCDifferential(DifferentialPair::PAIR_01);
 
   Serial.print("Differential: ");
   Serial.print(results);
   Serial.print("(");
-  Serial.print(results * multiplier);
-  Serial.println("mV)");
+  Serial.print(ads.computeVolts(results));
+  Serial.println("V)");
 
   delay(1000);
 }
--- examples/singleended/singleended.ino
+++ examples/singleended/singleended.ino
@@ -18,14 +18,14 @@ void setup(void) {
   // functions, but be careful never to exceed VDD +0.3V max, or to
   // exceed the upper and lower limits if you adjust the input range!
   // Setting these values incorrectly may destroy your ADC!
-  //                                                                ADS1015  ADS1115
-  //                                                                -------  -------
-  // ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
-  // ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
-  // ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
-  // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
-  // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
-  // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
+  //                                                                       ADS1015  ADS1115
+  //                                                                       -------  -------
+  // ads.setGain(Gain::TWOTHIRDS_6144MV); // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
+  // ads.setGain(Gain::ONE_4096MV);       // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
+  // ads.setGain(Gain::TWO_2048MV);       // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
+  // ads.setGain(Gain::FOUR_1024MV);      // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
+  // ads.setGain(Gain::EIGHT_512MV);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
+  // ads.setGain(Gain::SIXTEEN_256MV);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
 
   ads.begin();
   ads.setGain(Gain::TWOTHIRDS_6144MV);
